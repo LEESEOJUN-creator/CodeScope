103c51c7376b87e1074cb797faebb89fe815699b
이서준 <seojun7988@naver.com>
2026-06-23 17:53:32 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/codescope/CodescopeApplication.java
@@ -0,0 +1,13 @@
+package com.codescope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodescopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodescopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codescope/CodescopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codescope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodescopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
